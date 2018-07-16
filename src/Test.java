be856c6eec873861ed05ccd47e1a431ce84dde41
Agata <twardowska123@gmail.com>
2018-07-16 18:22:45 +0200

    zadanie 2 prezentacja, poprawka

--- src/Test.java
+++ src/Test.java
@@ -14,20 +14,20 @@ public class Test {
         System.out.println("Podaj wypłate");
         double wyplata = scan.nextDouble();
         scan.nextLine();
+
+        emp[0] = new Employee(imie,nazwisko,wyplata);
+
         System.out.println("Podaj imię");
         String imie1 = scan.nextLine();
         System.out.println("Podaj nazwisko");
         String nazwisko1 = scan.nextLine();
         System.out.println("Podaj wypłate");
         double wyplata1 = scan.nextDouble();
-
-
-
-        emp[0] = new Employee(imie,nazwisko,wyplata);
         emp[1] = new Employee(imie1,nazwisko1,wyplata1);
 
         double suma = emp[0].getWyplata() + emp[1].getWyplata();
-        System.out.println(emp[0].getWyplata() + "+" + emp[1].getWyplata()+ "+" + "=" + suma);
+        
+        System.out.println("Łącznie na wypłaty wydawane jest" + emp[0].getWyplata() + "+" + emp[1].getWyplata()+ "+" + "=" + suma);
 
     }
 
